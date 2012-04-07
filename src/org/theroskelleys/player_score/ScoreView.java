3ff93416a8cbd78ff05bb4fd85291a88aabf7907
Jordan Roskelley <jordan.roskelley@gmail.com>
2012-04-07 02:02:07 -0600

    got names printing on score view, looked into getting edittexts on there too... but it was causing problems, so i commented that out for now. goodnight

--- src/org/theroskelleys/player_score/ScoreView.java
+++ src/org/theroskelleys/player_score/ScoreView.java
@@ -1,57 +1,81 @@
 package org.theroskelleys.player_score;
 
-import com.google.gson.Gson;
-
-import android.app.*;
+import android.app.Activity;
 import android.content.SharedPreferences;
-import android.os.*;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class ScoreView extends Activity
-{
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-	{
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.score_view);
-        
-        TableLayout tl = (TableLayout)findViewById(R.id.tbl_scores);
-        
-        TableRow tr = new TableRow(this);
-        tr.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
-        
-        String[] chosenNames = getChosenNames();
-        View cornerPlaceholder = new View(this);
-        tr.addView(cornerPlaceholder);
-        for(String name:chosenNames) {
-            TextView tv = new TextView(this);
-            tv.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
-            tv.setText(name);
-            tr.addView(tv);
-        }
-        
-        tl.addView(tr, new TableLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
-        tl.invalidate();
-    }
-    
-    private String[] getChosenNames() {
-    	SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-    	String jsonChosenNames = sp.getString("chosenNames", "");
-        Toast.makeText(this, jsonChosenNames, Toast.LENGTH_SHORT).show();
-    	Gson g = new Gson();
-    	String[] chosenNames = g.fromJson(jsonChosenNames, String[].class);
-    	return chosenNames;
-    }
-    
-    public void buttonHandler(View v){
-    	switch(v.getId()){
-    	
-    	}
-    }
+import com.google.gson.Gson;
+
+public class ScoreView extends Activity {
+	TableLayout tl;
+	int roundNumber, numPlayers;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.score_view);
+
+		tl = (TableLayout) findViewById(R.id.tbl_scores);
+		printNames();
+		paintScoreEntryFields();
+		roundNumber = 1;
+	}
+
+	/*
+	 * What to do... Have array of objects to represent the table, and repaint
+	 * between entries? - not efficient...
+	 */
+
+	public void buttonHandler(View v) {
+		switch (v.getId()) {
+
+		}
+	}
+	
+	private void paintScoreEntryFields() {/*
+		LinearLayout ll = (LinearLayout)findViewById(R.id.ll_entry);
+		View cornerPlaceholder = new View(this);
+		ll.addView(cornerPlaceholder);
+		
+		for(int i = 0; i < numPlayers; i++) {
+			//add entry
+			EditText et = new EditText(this);
+			//TODO possibly hold an array of et's in memory, and then when they finish a round, just loop that array?
+			et.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+			ll.addView(et);
+		}
+		*/
+	}
+
+	private void printNames() {
+		TableRow tr = new TableRow(this);
+		String[] chosenNames = getChosenNames();
+		numPlayers = chosenNames.length;
+		View cornerPlaceholder = new View(this);
+		tr.addView(cornerPlaceholder);
+		for (String name : chosenNames) {
+			TextView tv = new TextView(this);
+			tv.setText(name);
+			tr.addView(tv);
+		}
+		tl.addView(tr, new TableLayout.LayoutParams(LayoutParams.MATCH_PARENT,
+				LayoutParams.WRAP_CONTENT));
+	}
+
+	private String[] getChosenNames() {
+		SharedPreferences sp = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		String jsonChosenNames = sp.getString("chosenNames", "");
+		Gson g = new Gson();
+		String[] chosenNames = g.fromJson(jsonChosenNames, String[].class);
+		return chosenNames;
+	}
 }
