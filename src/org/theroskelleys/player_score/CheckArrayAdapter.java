912ca7eff93efc4be50da555ba83dff9bc3a5826
Jordan Roskelley <jordan.roskelley@gmail.com>
2012-04-07 01:17:32 -0600

    oops, minor update

--- src/org/theroskelleys/player_score/CheckArrayAdapter.java
+++ src/org/theroskelleys/player_score/CheckArrayAdapter.java
@@ -4,13 +4,11 @@ import java.util.List;
 import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class CheckArrayAdapter extends ArrayAdapter<CheckRowItem>
 {
